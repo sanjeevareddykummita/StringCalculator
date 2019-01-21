825e57846cefd494c5ec7084ab35cfd91c1a0a69
unknown <sanjered@DIN86000533.corp.capgemini.com>
2019-01-21 17:10:46 +0530

    Initial commit

---
+++ StringCalculator/StringCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter numbers\n");
+            string numbers = Console.ReadLine();
+
+        }
+
+    }
+}
