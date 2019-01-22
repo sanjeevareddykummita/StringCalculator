b4f31575e561a23e723f2e3968a6b0a8427936e7
unknown <sanjered@DIN86000533.corp.capgemini.com>
2019-01-22 12:58:25 +0530

    Added method to Support different delimiters

--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -9,29 +9,72 @@ namespace StringCalculator
     public class Stringcalculator
     {
         private readonly List<string> _seperators = new List<string> { ",", "\n" };
+        private const string _customseperators = "//";
+        private const int _startIndexOfNumbersIncludingCustomSeperator = 3; 
+        private const int _startIndexOfCustomSeperator = 2; 
 
         public int Add(string numbers)
         {
             if (String.IsNullOrWhiteSpace(numbers)) return 0;
 
+            if (numbers.StartsWith(_customseperators))
+            {
+                numbers = GetNumbersExcludingCustomSeperators(numbers);
+            }
             numbers = ReplaceBackslashN(numbers);
 
             return GetSumOfNumbers(numbers);
         }
-
+        
         private int GetSumOfNumbers(string numbers)
         { 
             if (numbers.Contains("\""))
             {
                   numbers = numbers.Replace("\"", "0");
-                
             }
+
+            if (numbers.IndexOf("\n") == 0)
+            {
+                numbers = numbers.Substring(1);
+            }
+
             if (!String.IsNullOrEmpty(numbers.Trim()))
             {
                 var lstNumbers = numbers.Split(_seperators.ToArray(), StringSplitOptions.None).Select(int.Parse).ToList();
                 return lstNumbers.Sum();
             }
             return 0;
+        }
+
+        private string GetNumbersExcludingCustomSeperators(string numbers)
+        {
+            var startIndexOfString = IncludeCustomSeperatorAndReturnStartIndexOfNumbers(numbers);
+
+            return numbers.Substring(startIndexOfString);
+        }
+
+        private int IncludeCustomSeperatorAndReturnStartIndexOfNumbers(string numbers)
+        {
+            var customSeperators = GetCustomSeperators(numbers); 
+            _seperators.AddRange(customSeperators);
+            
+            return _startIndexOfNumbersIncludingCustomSeperator;
+        }
+
+        private static IList<string> GetCustomSeperators(string numbers)
+        {
+            numbers = ReplaceBackslashN(numbers);
+
+            var allSeperators = numbers.Substring(_startIndexOfCustomSeperator, numbers.IndexOf("\n") - _startIndexOfCustomSeperator);
+
+            var splitSeperators = allSeperators.Split('[').Select(x => x.TrimEnd(']')).ToList(); 
+
+            if (splitSeperators.Contains(string.Empty))
+            {
+                splitSeperators.Remove(string.Empty);
+            }
+
+            return splitSeperators;
         }
 
         // When we enter 1\n2 System is treated as 1\\n2 . To reduce the double to single slash n by using below method.
--- StringCalculator/StringCalculatorUnitTests/StringCalculatorTests.cs
+++ StringCalculator/StringCalculatorUnitTests/StringCalculatorTests.cs
@@ -19,7 +19,7 @@ namespace StringCalculatorUnitTests
         [TestCase(0, ExpectedResult = 0)]
         [TestCase(null, ExpectedResult = 0)]
         [TestCase("", ExpectedResult = 0)]
-        public void Returns_0_When_0_Or_Null_Or_Empty(string numbers, int result)
+        public void Returns_Zero_When_Zero_Or_Null_Or_Empty(string numbers, int result) 
         {
             int actual = GetSumOfNumbers(numbers);
 
@@ -27,17 +27,29 @@ namespace StringCalculatorUnitTests
         }
 
         [Test]
-        [TestCase("3", ExpectedResult = 3)]
-        [TestCase("5,10", ExpectedResult = 15)]
-        [TestCase("110,210,310", ExpectedResult = 630)]
-        public void Returns_Sum_When_ValidNumbersEnteredIn_String(string numbers, int result)
+        [TestCase("1", ExpectedResult = 1)]
+        [TestCase("6,10", ExpectedResult = 16)]
+        [TestCase("50,100,200", ExpectedResult = 350)]
+        public void Returns_Sum_When_ValidNumbersEntered_String(string numbers, int result) 
         {
             int actual = GetSumOfNumbers(numbers);
 
             Assert.AreEqual(result, actual);
         }
-        
-        
+
+        [Test]
+        [TestCase("//;\n1;2;3", ExpectedResult = 6)]
+        [TestCase("//;\n12;20;25", ExpectedResult = 57)]
+        [TestCase("//$\n2$6$12", ExpectedResult = 20)]
+        [TestCase("//$\n5$15$50", ExpectedResult = 60)]
+        public void Returns_Sum_When_CustomSeperatorPrefixed_Numbers(string numbers, int result) 
+        {
+            int actual = GetSumOfNumbers(numbers);
+
+            Assert.AreEqual(result, actual);
+        }
+
+        //Method calls from StringCalculator class
         private int GetSumOfNumbers(string numbers)
         {
             var calculatedResult = stringCalc.Add(numbers);
